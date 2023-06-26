c887b2694a2a3d78fb6ba81a192c46324319fa53
zharie3 <137117666+zharie3@users.noreply.github.com>
2023-06-26 15:08:09 -0400

    Update hello.py
    step 2.1

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello World!")
 print("\t This is my first Git repository on GitHub.")
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+
+name = "Julia"
+print_message(name)
